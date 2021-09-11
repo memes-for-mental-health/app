5bcb1789e03c207191c4f53cfcf341afde334e86
neilZon <neilcviloria@gmail.com>
2021-09-11 10:41:23 -0600

    add fix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Signup from './pages/Signup/Signup';
 import Login from './pages/Login/Login';
 
 function App() {
   return (
     <Router>
-      <Route path="/login">
-        <Login />
-      </Route>
-      <Route path="/signup">
-        <Signup />
-      </Route>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+      </Switch>
     </Router>
   );
 }
