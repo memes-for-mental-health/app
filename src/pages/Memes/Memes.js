d80dd4b970a9113cc0650b7819d98cc3acc2ca70
altaafackbar <Altaaf01>
2021-09-11 14:11:08 -0600

    memes page

--- src/pages/Memes/Memes.js
+++ src/pages/Memes/Memes.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 const Memes = () => {
-  return <div>Memes</div>;
+  const [data, setData] = useState([]);
+
+  useEffect(async () => {
+    const response = await axios.get('https://www.reddit.com/r/memes/hot/.json?count=20');
+    setData(response.data.data.children)
+  }, []);
+
+  
+
+  return <div className="memes">Memes
+    {data.map((item, idx) => {
+      console.log(item)
+      return (
+        <>
+        <img  key={idx} src={item.data.url_overridden_by_dest} />
+        
+        </>
+      )
+    })}
+  </div>;
 };
 
 export default Memes;
