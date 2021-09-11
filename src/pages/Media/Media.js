2175d39e9bfc6f10890ec4f978e7365b851f8748
neilZon <neilcviloria@gmail.com>
2021-09-11 15:41:12 -0600

    style login page

--- src/pages/Media/Media.js
+++ src/pages/Media/Media.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import List from '../../components/List/List';
+
+import styles from './Media.module.css';
 
 const Media = () => {
-  return <div>media</div>;
+  const data = [
+    { data: 'Movie' },
+    { data: 'Video' },
+    { data: 'Quote' },
+    { data: 'Books' },
+  ];
+
+  return (
+    <div className={styles.media}>
+      <div className={styles.container}>
+        <div>What you interested in?</div>
+        <List listItems={data} />
+      </div>
+    </div>
+  );
 };
 
 export default Media;
