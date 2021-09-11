16c296c7bee48b39fdf57a8297f99c049a7a76df
altaafackbar <Altaaf01>
2021-09-11 15:42:52 -0600

    memes

--- src/pages/Memes/Memes.js
+++ src/pages/Memes/Memes.js
@@ -3,9 +3,13 @@ import axios from 'axios';
 
 const Memes = () => {
   const [data, setData] = useState([]);
+  const moods = ['sad', 'angry']
+  const links = {'sad': 'https://www.reddit.com/r/aww/hot/.json?limit=40'
+                ,'angry':'https://www.reddit.com/r/memes/hot/.json?limit=40'
+                ,'depressed':'https://www.reddit.com/r/2meirl4meirl/hot/.json?limit=40'}
 
   useEffect(async () => {
-    const response = await axios.get('https://www.reddit.com/r/memes/hot/.json?count=20');
+    const response = await axios.get('https://www.reddit.com/r/aww/hot/.json?limit=40');
     setData(response.data.data.children)
   }, []);
 
@@ -16,7 +20,8 @@ const Memes = () => {
       console.log(item)
       return (
         <>
-        <img  key={idx} src={item.data.url_overridden_by_dest} />
+        <img  key={idx} src={item.data.url_overridden_by_dest} width="400" 
+     height="500"/>
         
         </>
       )
