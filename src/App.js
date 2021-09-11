30d6b191f3f1cc5ad886e183d38a03f44998fa53
neilZon <neilcviloria@gmail.com>
2021-09-11 10:51:07 -0600

    media and mood pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Signup from './pages/Signup/Signup';
 import Login from './pages/Login/Login';
+import Mood from './pages/Mood/Mood';
+import Media from './pages/Media/Media';
 
 function App() {
   return (
@@ -12,6 +14,12 @@ function App() {
         </Route>
         <Route path="/signup">
           <Signup />
+        </Route>
+        <Route path="/mood">
+          <Mood />
+        </Route>
+        <Route path="/media">
+          <Media />
         </Route>
       </Switch>
     </Router>
---
+++ src/pages/Mood/Mood.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Mood = () => {
+  return <div>mood</div>;
+};
+
+export default Mood;
