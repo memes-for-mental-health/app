eba91a6f6663aa84ef8bd22f6fa7c57360a9f053
Najiib <maxamednajiib10@hotmail.com>
2021-09-11 13:01:15 -0600

    add login with google button

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import GoogleLogin from 'react-google-login';
 
-const Login = () => {
-  return <div>login</div>;
-};
+class GoogleSocialAuth extends Component {
 
-export default Login;
+  render() {
+    const googleResponse = (response) => {
+      console.log(response);
+    }
+
+
+  var responseGoogle = () => {
+    console.log("U clicked me")
+  }
+
+    return (
+      <div className="App">
+        <h1>LOGIN WITH GOOGLE</h1>
+      
+        <GoogleLogin
+          clientId="<Google Client ID>"
+          buttonText="LOGIN WITH GOOGLE"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+        />
+      </div>
+    );
+  }
+}
+
+export default GoogleSocialAuth;
